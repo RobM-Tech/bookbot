acb408862eebfff433df408ba4aba7aabd03f9fe
RobM_Tech <robert.mathieu.tech@gmail.com>
2025-05-05 22:43:01 -0500

    D: added char speration and count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_word_count
+from stats import get_word_count, get_char_count
+
 
 # Functions
 
@@ -17,7 +18,9 @@ def main(): ## main function
     #
     book_text = get_book_text("books/frankenstein.txt") 
     word_count = get_word_count(book_text)
+    char_count = get_char_count(book_text)
     print(f"{word_count} words found in the document.")
+    print(f"{char_count}")
     return book_text
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+
+
 ## data analysis ##
 
 def get_word_count(book_text): ## get book_text from main()
@@ -5,3 +7,15 @@ def get_word_count(book_text): ## get book_text from main()
     words = book_text.split()    ## split text
     word_count = len(words) ## count words
     return word_count       ## returns word count
+
+def get_char_count(book_text): ## gets total count of characters
+    #
+    char_dic = {}
+    
+    for l in book_text.lower():
+        if l not in char_dic:
+            char_dic[l] = 1
+        else:
+            char_dic[l] += 1
+    return char_dic
+            
