5fe6b0836af5d33c728c5ab8b1c53e1fb01c7fce
RobM_Tech <robert.mathieu.tech@gmail.com>
2025-05-06 12:17:31 -0500

    E: Ordered list, removed none letters

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from stats import get_word_count, get_char_count
-
+from stats import get_word_count, get_char_count, char_sorter
+import sys
 
 # Functions
 
 ## get book text from file ##
 
-def get_book_text(file_path):## Read the book text from the file
-    #
+def get_book_text(file_path):
+    ## get file
     with open(file_path) as f:
-        book_text = f.read() ## converts file to text
+        book_text = f.read() ## converts to text
     return book_text         ## returns the text
 
 
@@ -17,11 +17,25 @@ def get_book_text(file_path):## Read the book text from the file
 def main(): ## main function
     #
     book_text = get_book_text("books/frankenstein.txt") 
-    word_count = get_word_count(book_text)
-    char_count = get_char_count(book_text)
-    print(f"{word_count} words found in the document.")
-    print(f"{char_count}")
+    word_count = get_word_count(book_text) # gets word count
+    char_count = get_char_count(book_text) # gets char count
+    sorted_char_count = char_sorter(char_count) # sorts chars by count
+    ## print results
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words.")
+    print("--------- Character Count -------")
+    
+    for item in sorted_char_count:
+        print(f"{item['char']}: {item['count']}") # prints sorted char count
+
+    print("============= END ===============")
     return book_text
+
+    
+
+    
 
 if __name__ == "__main__":
     main()    
--- stats.py
+++ stats.py
@@ -2,20 +2,43 @@
 
 ## data analysis ##
 
-def get_word_count(book_text): ## get book_text from main()
-    #
-    words = book_text.split()    ## split text
-    word_count = len(words) ## count words
-    return word_count       ## returns word count
+def get_word_count(book_text):
+    ## Counts the number of words in the provided text.
+    words = book_text.split()    ## split text into words
+    word_count = len(words)      ## get word count
+    return word_count            ## return word count
 
-def get_char_count(book_text): ## gets total count of characters
-    #
-    char_dic = {}
+def get_char_count(book_text):
+    ## Counts the occurrences of each character in the provided text.
+    char_dic = {} ## empty dictionary to store characters/counts
     
+    ## loop through text, convert to lowercase , count ea char, add to dic
     for l in book_text.lower():
         if l not in char_dic:
-            char_dic[l] = 1
+            char_dic[l] = 1     ## if not in dic add and +1 count
         else:
-            char_dic[l] += 1
-    return char_dic
-            
+            char_dic[l] += 1    ## +1 to characters count
+
+    return char_dic ## return result to dictionary
+
+
+
+
+
+def char_sorter(char_dic):
+    #
+    filter_dic = {k: v for k, v in char_dic.items() if k.isalpha()}
+
+    char_list = [{"char": k, "count": v} for k, v in filter_dic.items()]
+    
+    def sort_on(char_dic):
+        return char_dic["count"]
+    
+    char_list.sort(key=sort_on, reverse=True)
+    
+    return char_list
+
+
+
+    
+   
